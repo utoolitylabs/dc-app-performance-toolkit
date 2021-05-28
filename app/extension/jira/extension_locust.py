af40383cdd93532f18c6dcc5f2678606af680c91
Steffen Opel <sopel@utoolity.net>
2021-05-28 18:43:49 +0200

    IFAWS-1507: Add app specific Locust action

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -2,28 +2,16 @@ import re
 from locustio.common_utils import init_logger, jira_measure, run_as_specific_user  # noqa F401
 
 logger = init_logger(app_type='jira')
+FIRST_CONNECTOR = "add4aa6b-2b7f-473d-ab7b-1d2bcb76830e"
 
-
-@jira_measure("locust_app_specific_action")
-# @run_as_specific_user(username='admin', password='admin')  # run as specific user
+@run_as_specific_user(username='admin', password='admin')  # run as specific user
 def app_specific_action(locust):
-    r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
-    content = r.content.decode('utf-8')   # decode response content
-
-    token_pattern_example = '"token":"(.+?)"'
-    id_pattern_example = '"id":"(.+?)"'
-    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
-    id = re.findall(id_pattern_example, content)    # get ID from response using regexp
-
-    logger.locust_info(f'token: {token}, id: {id}')  # log info for debug when verbose is true in jira.yml file
-    if 'assertion string' not in content:
-        logger.error(f"'assertion string' was not found in {content}")
-    assert 'assertion string' in content  # assert specific string in response content
-
-    body = {"id": id, "token": token}  # include parsed variables to POST request body
-    headers = {'content-type': 'application/json'}
-    r = locust.post('/app/post_endpoint', body, headers, catch_response=True)  # call app-specific POST endpoint
-    content = r.content.decode('utf-8')
-    if 'assertion string after successful POST request' not in content:
-        logger.error(f"'assertion string after successful POST request' was not found in {content}")
-    assert 'assertion string after successful POST request' in content  # assertion after POST request
+    
+    @jira_measure("locust_app_api_get_temporary_credentials")
+    def get_temporary_credentials():
+        path = "/rest/identity-federation-for-aws/2.2/connectors/" + FIRST_CONNECTOR + "/credentials"
+        r = locust.get(path, catch_response=True)  # call app-specific GET endpoint
+        
+        assert r.status_code == 200, f"expected GET .../credentials 200, got {r.status_code}"
+        
+    get_temporary_credentials()
