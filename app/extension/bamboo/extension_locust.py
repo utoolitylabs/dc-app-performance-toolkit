74f87e7fc3ecd5381489df4119f2633a6eb0e3bf
Henrik Opel <hopel@utoolity.net>
2022-02-18 14:27:17 +0100

    IFAWS-1573: Adjust locust custom actions to run in parallel

--- app/extension/bamboo/extension_locust.py
+++ app/extension/bamboo/extension_locust.py
@@ -9,26 +9,22 @@ FIRST_CONNECTOR = "f70a97a6-5362-4dec-8fbc-630ee925d1af"
 # NOTE: Quite a deviation from the original sample, but that would not cater for multiple custom actions without changes to dc toolkit core logic,
 # hence starting with on custom action invoking multiple measurements in itself (to be revised if results are not satisfactory)
 # @run_as_specific_user(username='admin', password='admin')
-def app_specific_action(locust):
+@bamboo_measure("locust_app_api_get_temporary_credentials")
+def app_specific_action_get_temporary_credential(locust):
     
-    @bamboo_measure("locust_app_api_get_temporary_credentials")
-    def get_temporary_credentials():
-        # logger.info(f"'get_temporary_credentials() called ...")
-        path = "/rest/identity-federation-for-aws/2.2/connectors/" + FIRST_CONNECTOR + "/credentials"
-        r = locust.get(path, catch_response=True)  # call app-specific GET endpoint
-        logger.info(f"'... locust.get() returned status code {r.status_code} ...")
-        
-        assert r.status_code == 200, f"expected GET .../credentials 200, got {r.status_code}"
+    # logger.info(f"'get_temporary_credentials() called ...")
+    path = "/rest/identity-federation-for-aws/2.2/connectors/" + FIRST_CONNECTOR + "/credentials"
+    r = locust.get(path, catch_response=True)  # call app-specific GET endpoint
+    logger.info(f"'... locust.get() returned status code {r.status_code} ...")
+    
+    assert r.status_code == 200, f"expected GET .../credentials 200, got {r.status_code}"
     
     
-    @bamboo_measure("locust_app_api_get_ecr_credentials")
-    def get_ecr_credentials():
-        # logger.info(f"'get_ecr_credentials() called ...")
-        path = "/rest/identity-federation-for-aws/2.2/connectors/" + FIRST_CONNECTOR + "/credentials"
-        r = locust.get(path, catch_response=True)  # call app-specific GET endpoint
-        logger.info(f"'... locust.get() returned status code {r.status_code} ...")
-        
-        assert r.status_code == 200, f"expected GET .../credentials 200, got {r.status_code}"
+@bamboo_measure("locust_app_api_get_ecr_credentials")
+def app_specific_action_get_ecr_credential(locust):
+    # logger.info(f"'get_ecr_credentials() called ...")
+    path = "/rest/identity-federation-for-aws/2.2/connectors/" + FIRST_CONNECTOR + "/credentials"
+    r = locust.get(path, catch_response=True)  # call app-specific GET endpoint
+    logger.info(f"'... locust.get() returned status code {r.status_code} ...")
     
-    get_temporary_credentials()
-    get_ecr_credentials()
+    assert r.status_code == 200, f"expected GET .../credentials 200, got {r.status_code}"
