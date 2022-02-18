fe0f1d1ffbb5a30455df611020508b8719fc4c9a
Henrik Opel <hopel@utoolity.net>
2022-02-18 14:48:01 +0100

    IFAWS-1573: Add logging of calling user name to custom action invocations
    NOTE: Triggered by still encountering a few 403 responses to otherwise identical calls during test runs.

--- app/extension/bamboo/extension_locust.py
+++ app/extension/bamboo/extension_locust.py
@@ -2,9 +2,7 @@ import re
 from locustio.common_utils import init_logger, bamboo_measure, run_as_specific_user  # noqa F401
 
 logger = init_logger(app_type='bamboo')
-
-logger = init_logger(app_type='bamboo')
-FIRST_CONNECTOR = "f70a97a6-5362-4dec-8fbc-630ee925d1af"
+FIRST_CONNECTOR = "53f911b5-3613-42cf-8d5f-ef9535051673"
 
 # NOTE: Quite a deviation from the original sample, but that would not cater for multiple custom actions without changes to dc toolkit core logic,
 # hence starting with on custom action invoking multiple measurements in itself (to be revised if results are not satisfactory)
@@ -12,7 +10,8 @@ FIRST_CONNECTOR = "f70a97a6-5362-4dec-8fbc-630ee925d1af"
 @bamboo_measure("locust_app_api_get_temporary_credentials")
 def app_specific_action_get_temporary_credential(locust):
     
-    # logger.info(f"'get_temporary_credentials() called ...")
+    userName = locust.session_data_storage["username"]
+    logger.info(f"'get_temporary_credentials() called for user {userName} ...")
     path = "/rest/identity-federation-for-aws/2.2/connectors/" + FIRST_CONNECTOR + "/credentials"
     r = locust.get(path, catch_response=True)  # call app-specific GET endpoint
     logger.info(f"'... locust.get() returned status code {r.status_code} ...")
@@ -22,7 +21,9 @@ def app_specific_action_get_temporary_credential(locust):
     
 @bamboo_measure("locust_app_api_get_ecr_credentials")
 def app_specific_action_get_ecr_credential(locust):
-    # logger.info(f"'get_ecr_credentials() called ...")
+
+    userName = locust.session_data_storage["username"]
+    logger.info(f"'get_ecr_credentials() called for user {userName} ...")
     path = "/rest/identity-federation-for-aws/2.2/connectors/" + FIRST_CONNECTOR + "/credentials"
     r = locust.get(path, catch_response=True)  # call app-specific GET endpoint
     logger.info(f"'... locust.get() returned status code {r.status_code} ...")
