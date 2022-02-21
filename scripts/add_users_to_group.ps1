821fe2099554942ec5119ce4a34175cb584a0821
Henrik Opel <hopel@utoolity.net>
2022-02-21 18:02:52 +0100

    IFAWS-1573: Add on the fly creation of desired group to add user script

--- scripts/add_users_to_group.ps1
+++ scripts/add_users_to_group.ps1
@@ -1,18 +1,32 @@
 param (
     [string]$targetBaseUrl = "https://bamboo.ifaws-1573-20220216-1.tst.utoolity.net",
-    [string]$groupName = "bamboo-user",
+    [string]$groupName = "performance-user",
     [string]$user = "admin",
     [string]$pass = $( Read-Host "Enter host instance password" )
 )
 
 $sessionName = [guid]::NewGuid().toString()
 $currentUserEndpoint = $targetBaseUrl + "/rest/api/latest/currentUser"
+$adminGroupsEndpoint = $targetBaseUrl + "/rest/api/latest/admin/groups"
 $getUsersEndpoint = $targetBaseUrl + "/rest/api/latest/admin/users"
-$addUsersEndpoint = $targetBaseUrl + "/rest/api/latest/admin/groups/performance-user/add-users"
+$addUsersEndpoint = $targetBaseUrl + "/rest/api/latest/admin/groups/" + $groupName +"/add-users"
 
 # Prime the session
 $auth = $user + ":" + $pass
 http --auth $auth --session=$sessionName GET $currentUserEndpoint
+
+# Create group, if not there already
+$getGroupsResponse = http --session=$sessionName --json GET $adminGroupsEndpoint
+$groupNamesOnlyArray = $getGroupsResponse | jq '[.results[] | {name} | .name]'
+Write-Output $groupNamesOnlyArray
+if($groupNamesOnlyArray -Match $groupName) {
+    Write-Output "Found group name '"  $groupName  "' using that ..."
+}
+else {
+    Write-Output "Did not find group name '"  $groupName  "' creating ..."
+    $payload = '{"name": "' + $groupName + '"}'
+    Write-Output $payload | http --session=$sessionName --json POST $adminGroupsEndpoint
+}
 
 # Grab users and put them into groups in chunks to prevent hibernate constraint violations (presumed overload)
 $chunkSize = 32
