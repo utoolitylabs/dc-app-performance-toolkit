3ef4f514e4c3cc5f7dd79be37e8c636c6dcdbc73
Henrik Opel <hopel@utoolity.net>
2022-02-24 17:38:49 +0100

    AAWS-935: : Adjust role to assume for custom task testing

--- scripts/create-ifaws-connectors.ps1
+++ scripts/create-ifaws-connectors.ps1
@@ -52,7 +52,7 @@ $connectorTemplate = @"
     "scope": "SYSTEM",
     "partition": "aws",
     "accountId": "ACCOUNT_ID_TBR",
-    "roleArn": "arn:aws:iam::288727192237:role/ifaws-quickstart-uaa-453-IdentityFederationForAWS-1L84NAN9NUAYQ",
+    "roleArn": "arn:aws:iam::288727192237:role/aaws-quickstart-uaa-459-1-AutomationWithAWSCoreRol-4PDHYQ9SSNBL",
     "externalId": "",
     "iamPolicy": "",
     "groups": [
