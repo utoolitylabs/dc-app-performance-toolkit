029dc8f4617e87c1dde66333a79ad462f2896d18
Henrik Opel <hopel@utoolity.net>
2022-06-09 19:57:58 +0200

    AAWS-932: Adjust custom test to trigger side effect from skipped first quick create test run
    (cherry picked from commit fdbb52024cabc3c715917472cd2e3a780bd90d95)

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -20,6 +20,51 @@ def app_specific_action_invoke_aws_api_action(locust):
     project = ["VLLR","10359"]
     project_id = project[1]
 
+    # NOTE: Copied quick create and kept it s execution, just not measuring it, to ensureside effects needed by the create test below
+    # @jira_measure('locust_create_issue:open_quick_create')
+    def create_issue_open_quick_create():
+        raise_if_login_failed(locust)
+
+        # 200 /secure/QuickCreateIssue!default.jspa?decorator=none
+        r = locust.post('/secure/QuickCreateIssue!default.jspa?decorator=none', ADMIN_HEADERS, catch_response=True)
+
+        content = r.content.decode('utf-8')
+        atl_token = fetch_by_re(params.atl_token_pattern, content)
+        form_token = fetch_by_re(params.form_token_pattern, content)
+        issue_type = fetch_by_re(params.issue_type_pattern, content)
+        resolution_done = fetch_by_re(params.resolution_done_pattern, content)
+        fields_to_retain = re.findall(params.fields_to_retain_pattern, content)
+        custom_fields_to_retain = re.findall(params.custom_fields_to_retain_pattern, content)
+
+        issue_body_params_dict = {'atl_token': atl_token,
+                                  'form_token': form_token,
+                                  'issue_type': issue_type,
+                                  'project_id': project_id,
+                                  'resolution_done': resolution_done,
+                                  'fields_to_retain': fields_to_retain,
+                                  'custom_fields_to_retain': custom_fields_to_retain
+                                  }
+
+        if not ('"id":"project","label":"Project"' in content):
+            logger.error(f'{params.err_message_create_issue}: {content}')
+        assert '"id":"project","label":"Project"' in content, params.err_message_create_issue
+
+        # 205 /rest/quickedit/1.0/userpreferences/create
+        locust.post('/rest/quickedit/1.0/userpreferences/create',
+                    json=params.user_preferences_payload,
+                    headers=ADMIN_HEADERS,
+                    catch_response=True)
+
+        # 210 /rest/analytics/1.0/publish/bulk
+        locust.post('/rest/analytics/1.0/publish/bulk',
+                    json=params.resources_body.get("210"),
+                    headers=RESOURCE_HEADERS,
+                    catch_response=True)
+
+        locust.session_data_storage['issue_body_params_dict'] = issue_body_params_dict
+
+    create_issue_open_quick_create()
+
     @jira_measure("locust_app_invoke_aws_api_action")
     def create_issue_submit_form():
         raise_if_login_failed(locust)
