1d474e8d3cf874db00ba17f6b67caee56ecc2f43
Henrik Opel <hopel@utoolity.net>
2022-06-09 18:13:59 +0200

    AAWS-1140: Reapply customizations from AAWS-1058, DC testing 2023
    Squashed cherry picked commits:
    AAWS-932: Adjust custom test to trigger side effect from skipped first quick create test run
    
    (cherry picked from commit fdbb52024cabc3c715917472cd2e3a780bd90d95)
    (cherry picked from commit 029dc8f4617e87c1dde66333a79ad462f2896d18) (+3 squashed commit)
    
    Squashed commit:
    
    [91998cc] AAWS-932: Adjust custom test to create issue in well known project
    
    (cherry picked from commit e3ab010634a697b4c4e7e06a769e498e76d76174)
    (cherry picked from commit 037cfb59266df2929b250bb98f12fc089794e83f)
    
    [08415fa] AAWS-932: Reduce IFAWS locust tests to one and convert it to an create issue clone for custom workflow actions
    
    (cherry picked from commit 08ba528afd3f3a1b8497aa33b3ea81c4af27858d)
    (cherry picked from commit 5127ad37f6f5f022e563b067e09224d102451e9e)
    
    [6a141fa] AAWS-1058: Disable custom selenium UI action
    
    NOTE: Cherry picked commit [a744909] to 'replay' AAWS adjustments onto slightly modified IFAWS baseline.
    (cherry picked from commit 1a4122e9e3832f419d03235048eb77aa135582c2)

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -1,34 +1,93 @@
+import random
 import re
-from locustio.common_utils import init_logger, jira_measure, run_as_specific_user, raise_if_login_failed  # noqa F401
+from locustio.jira.requests_params import Login, BrowseIssue, CreateIssue, SearchJql, ViewBoard, BrowseBoards, \
+    BrowseProjects, AddComment, ViewDashboard, EditIssue, ViewProjectSummary, jira_datasets
+from locustio.common_utils import jira_measure, fetch_by_re, timestamp_int, generate_random_string, TEXT_HEADERS, \
+    ADMIN_HEADERS, NO_TOKEN_HEADERS, RESOURCE_HEADERS, init_logger, raise_if_login_failed
+
 from util.conf import JIRA_SETTINGS
+import uuid
 
 logger = init_logger(app_type='jira')
+jira_dataset = jira_datasets()
 FIRST_CONNECTOR = JIRA_SETTINGS.ifaws_connector_id
 
-# NOTE: Quite a deviation from the original sample, but that would not cater for multiple custom actions without changes to dc toolkit core logic,
-# hence starting with on custom action invoking multiple measurements in itself (to be revised if results are not satisfactory)
-# @run_as_specific_user(username='admin', password='admin')
-@jira_measure("locust_app_api_get_temporary_credentials")
-def app_specific_action_get_temporary_credential(locust):
-    raise_if_login_failed(locust)
-    
-    userName = locust.session_data_storage["username"]
-    logger.info(f"'get_temporary_credentials() called for user {userName} ...")
-    path = "/rest/identity-federation-for-aws/2.2/connectors/" + FIRST_CONNECTOR + "/credentials"
-    r = locust.get(path, catch_response=True)  # call app-specific GET endpoint
-    logger.info(f"'... locust.get() returned status code {r.status_code} ...")
-    
-    assert r.status_code == 200, f"expected GET .../credentials 200, got {r.status_code}"
-    
-    
-@jira_measure("locust_app_api_get_ecr_credentials")
-def app_specific_action_get_ecr_credential(locust):
-    raise_if_login_failed(locust)
+def app_specific_action_invoke_aws_api_action(locust):
+    params = CreateIssue()
+    # NOTE: Can't use the random project choice here, as this would need adjustments of all used workflows, hence pinnig just one well known
+    # project = random.choice(jira_dataset['projects'])
+    # TODO: Adjust/verify - must be a valid project, and the one with the workflow adjusted to use our AAWS action
+    project = ["VLLR","10359"]
+    project_id = project[1]
 
-    userName = locust.session_data_storage["username"]
-    logger.info(f"'get_ecr_credentials() called for user {userName} ...")
-    path = "/rest/identity-federation-for-aws/2.2/connectors/" + FIRST_CONNECTOR + "/ecr/credentials"
-    r = locust.get(path, catch_response=True)  # call app-specific GET endpoint
-    logger.info(f"'... locust.get() returned status code {r.status_code} ...")
-    
-    assert r.status_code == 200, f"expected GET .../credentials 200, got {r.status_code}"
+    # NOTE: Copied quick create and kept it s execution, just not measuring it, to ensureside effects needed by the create test below
+    # @jira_measure('locust_create_issue:open_quick_create')
+    def create_issue_open_quick_create():
+        raise_if_login_failed(locust)
+
+        # 200 /secure/QuickCreateIssue!default.jspa?decorator=none
+        r = locust.post('/secure/QuickCreateIssue!default.jspa?decorator=none', ADMIN_HEADERS, catch_response=True)
+
+        content = r.content.decode('utf-8')
+        atl_token = fetch_by_re(params.atl_token_pattern, content)
+        form_token = fetch_by_re(params.form_token_pattern, content)
+        issue_type = fetch_by_re(params.issue_type_pattern, content)
+        resolution_done = fetch_by_re(params.resolution_done_pattern, content)
+        fields_to_retain = re.findall(params.fields_to_retain_pattern, content)
+        custom_fields_to_retain = re.findall(params.custom_fields_to_retain_pattern, content)
+
+        issue_body_params_dict = {'atl_token': atl_token,
+                                  'form_token': form_token,
+                                  'issue_type': issue_type,
+                                  'project_id': project_id,
+                                  'resolution_done': resolution_done,
+                                  'fields_to_retain': fields_to_retain,
+                                  'custom_fields_to_retain': custom_fields_to_retain
+                                  }
+
+        if not ('"id":"project","label":"Project"' in content):
+            logger.error(f'{params.err_message_create_issue}: {content}')
+        assert '"id":"project","label":"Project"' in content, params.err_message_create_issue
+
+        # 205 /rest/quickedit/1.0/userpreferences/create
+        locust.post('/rest/quickedit/1.0/userpreferences/create',
+                    json=params.user_preferences_payload,
+                    headers=ADMIN_HEADERS,
+                    catch_response=True)
+
+        # 210 /rest/analytics/1.0/publish/bulk
+        locust.post('/rest/analytics/1.0/publish/bulk',
+                    json=params.resources_body.get("210"),
+                    headers=RESOURCE_HEADERS,
+                    catch_response=True)
+
+        locust.session_data_storage['issue_body_params_dict'] = issue_body_params_dict
+
+    create_issue_open_quick_create()
+
+    @jira_measure("locust_app_invoke_aws_api_action")
+    def create_issue_submit_form():
+        raise_if_login_failed(locust)
+        issue_body = params.prepare_issue_body(locust.session_data_storage['issue_body_params_dict'],
+                                               user=locust.session_data_storage["username"])
+
+        # 215 /secure/QuickCreateIssue.jspa?decorator=none
+        r = locust.post('/secure/QuickCreateIssue.jspa?decorator=none',
+                        params=issue_body,
+                        headers=ADMIN_HEADERS,
+                        catch_response=True)
+
+        # 220 /rest/analytics/1.0/publish/bulk
+        locust.post('/rest/analytics/1.0/publish/bulk',
+                    json=params.resources_body.get("220"),
+                    headers=RESOURCE_HEADERS,
+                    catch_response=True)
+
+        content = r.content.decode('utf-8')
+        if '"id":"project","label":"Project"' not in content:
+            logger.error(f'{params.err_message_create_issue}: {content}')
+        assert '"id":"project","label":"Project"' in content, params.err_message_create_issue
+        issue_key = fetch_by_re(params.create_issue_key_pattern, content)
+        logger.locust_info(f"{params.action_name}: Issue {issue_key} was successfully created")
+
+    create_issue_submit_form()
